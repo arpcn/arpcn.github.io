29f77e236de2fe327d74d7c6beae6362e0daf94e
arpcn <arpcn@users.noreply.github.com>
2025-02-21 13:16:10 +0800

    Add files via upload

--- analytics.js
+++ analytics.js
@@ -3,7 +3,7 @@ const scriptTag = document.createElement('script');
 scriptTag.async = true;
 scriptTag.src = "https://www.googletagmanager.com/gtag/js?id=G-2V5RQ5SQ4W";
 
-scriptTag.onload = function() {   // 确保在脚本加载完成后再执行 gtag 配置
+scriptTag.onload = function() {   // Ensure to perform gtag configuration only after the script has fully loaded.
     const inlineScript = document.createElement('script');
     inlineScript.innerHTML = `
         window.dataLayer = window.dataLayer || [];
@@ -15,5 +15,4 @@ scriptTag.onload = function() {   // 确保在脚本加载完成后再执行 gta
 };
 
 document.head.appendChild(scriptTag);
-
 
