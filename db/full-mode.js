98b323910d7d5fa5c1d24b32004c7548e6cadfc4
arpcn <arpcn@users.noreply.github.com>
2026-02-06 10:13:34 +0800

    Add files via upload

--- db/full-mode.js
+++ db/full-mode.js
@@ -138,9 +138,12 @@
     
     // 确保路径以斜杠开头
     const normalizedPath = cleanPath.startsWith('/') ? cleanPath : `/${cleanPath}`;
+
+  // 使用相对路径，会 重定向到 arpcn.top
+  return `/access/39a393bd37490e3597370f63f89358a6${normalizedPath}.md`;
     
-    // 直接使用原始URL（如果fixScript能处理）：
-    return `https://publish-01.obsidian.md/access/${CONFIG.uid}${normalizedPath}.md`;
+    // 或者直接使用原始URL（如果fixScript能处理）：
+    // return `https://publish-01.obsidian.md/access/${CONFIG.uid}${normalizedPath}.md`;
   }
   
   // 立即显示内容
